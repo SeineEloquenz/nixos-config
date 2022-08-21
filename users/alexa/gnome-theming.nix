0099cd58006a6d1f93f73d21f4e6648af0d534c2
Alexander Linder <alexander.linder@mondcarion.group>
2022-08-21 14:38:55 +0200

    Update font

---
+++ users/alexa/gnome-theming.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+
+{
+  qt.enable = true;
+  qt.style = {
+    name = "kvantum";
+  };
+  home.sessionVariables = {
+    QT_STYLE_OVERRIDE = "kvantum";
+  };
+  xdg.configFile."Kvantum/kvantum.kvconfig".source = ~/.dotfiles/files/kvantum.kvconfig;
+  gtk.enable = true;
+  gtk.cursorTheme = {
+    name = "Numix-Cursor";
+    package = pkgs.numix-cursor-theme;
+  };
+  gtk.iconTheme = {
+    name = "Tela-dark";
+    package = pkgs.tela-icon-theme;
+  };
+  gtk.theme = {
+    name = "Layan-dark";
+    package = pkgs.layan-gtk-theme;
+  };
+  gtk.font = {
+    name = "Montserrat SemiBold";
+    package = pkgs.montserrat;
+  };
+
+  home.packages = with pkgs; [
+    qt5ct
+    libsForQt5.qtstyleplugin-kvantum
+  ];
+
+}
