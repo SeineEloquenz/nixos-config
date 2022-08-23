004b410bd12a6e6bfe52b8f7e8dce46e48bd0b6c
Alexander Linder <alexander.linder@mondcarion.group>
2022-08-23 22:37:02 +0200

    Remove hardware config

--- system/zenbook/configuration.nix
+++ system/zenbook/configuration.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      /etc/nixos/hardware-configuration.nix
       # Import base modules
       ../common.nix
       ../gnome-desktop.nix
