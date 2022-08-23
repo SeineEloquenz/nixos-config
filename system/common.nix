aa8cfe94cdf5a5bc3764c9fc72a0aa1bd9f7d207
Alexander Linder <alexander.linder@mondcarion.group>
2022-08-23 02:04:55 +0200

    Add ssh settings to global config

--- system/common.nix
+++ system/common.nix
@@ -29,6 +29,9 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  # Enable ssh agent
+  programs.ssh.startAgent = true;
+
   # Open ports in the firewall.
   networking.firewall.enable = false;
 
