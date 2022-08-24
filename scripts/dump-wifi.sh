c48ae927d3b487a7a758f5cb9208e52a85671bd2
Alexander Linder <alexander.linder@mondcarion.group>
2022-08-24 23:13:31 +0200

    Improve dump wifi script

--- scripts/dump-wifi.sh
+++ scripts/dump-wifi.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-grep -v '^[[:space:]]*$' /etc/NetworkManager/system-connections/$1.nmconnection | sed 's/^/        /'
+grep -v '^[[:space:]]*$' /etc/NetworkManager/system-connections/$1.nmconnection | sed 's/^/        /' | sed "1 s/^/    $1:\n/"
