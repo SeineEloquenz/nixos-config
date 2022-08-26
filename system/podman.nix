32fc25ae44ebf3488d88c69f1868d868cd3844a9
Alexander Linder <alexander.linder@mondcarion.group>
2022-08-27 00:04:06 +0200

    Add podman-compose

--- system/podman.nix
+++ system/podman.nix
@@ -1,7 +1,12 @@
-{ ... }:{
+{ pkgs, ... }:{
   virtualisation.podman = {
     enable = true;
     dockerCompat = true;
     enableNvidia = true;
+    defaultNetwork.dnsname.enable = true;
   };
+
+  environment.systemPackages = [
+    pkgs.podman-compose
+  ];
 }
