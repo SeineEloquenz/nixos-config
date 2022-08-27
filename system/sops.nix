2584dd01362103ec680bbf0af7ea65d0376918eb
Alexander Linder <alexander.linder@mondcarion.group>
2022-08-27 16:57:23 +0200

    Add sops to system

--- system/sops.nix
+++ system/sops.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... } : {
   imports = [ <sops-nix/modules/sops> ];
+
+  environment.systemPackages = [ pkgs.sops ];
+
   # This will add secrets.yml to the nix store
   # You can avoid this by adding a string to the full path instead, i.e.
   #sops.defaultSopsFile = "/root/.sops/secrets/example.yaml";
