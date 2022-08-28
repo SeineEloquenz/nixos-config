243a99a0292577c87a3e1f17fb7bbcdd4c08299e
Alexander Linder <alexander.linder@mondcarion.group>
2022-08-28 04:31:11 +0200

    Add drive

--- system/machines/mcgpres/drives.nix
+++ system/machines/mcgpres/drives.nix
@@ -16,4 +16,11 @@
 
   boot.initrd.luks.devices."luks-08a66126-62ed-4ffe-a8dd-ad7a9c61f29b".device = "/dev/disk/by-uuid/08a66126-62ed-4ffe-a8dd-ad7a9c61f29b";
 
+  # Data
+  fileSystems."/run/media/alexa/Data" = {
+      device = "/dev/disk/by-uuid/29aea319-492f-4a78-99e0-85d2147cb727";
+      fsType = "ext4";
+    };
+
+  boot.initrd.luks.devices."luks-ca7d7ccb-63ed-4c53-861c-1742536059cc".device = "/dev/disk/by-uuid/ca7d7ccb-63ed-4c53-861c-1742536059cc";
 }
