ad77ec0dc53ba7ac279181640e7fd45a81d3faf0
Alexander Linder <alexander.linder@mondcarion.group>
2022-08-30 00:37:34 +0200

    Theme gtk4 as well

--- users/alexa/gnome-theming.nix
+++ users/alexa/gnome-theming.nix
@@ -9,19 +9,29 @@
     QT_STYLE_OVERRIDE = "kvantum";
   };
   xdg.configFile."Kvantum/kvantum.kvconfig".source = ~/.dotfiles/files/kvantum.kvconfig;
+
   gtk.enable = true;
+
   gtk.cursorTheme = {
     name = "Numix-Cursor";
     package = pkgs.numix-cursor-theme;
   };
+
   gtk.iconTheme = {
     name = "Tela-dark";
     package = pkgs.tela-icon-theme;
   };
+
   gtk.theme = {
     name = "Layan-dark";
     package = pkgs.layan-gtk-theme;
   };
+  xdg.configFile."gtk-4.0/gtk.css".source = "${pkgs.layan-gtk-theme}/share/themes/Layan-dark/gtk-4.0/gtk.css";
+  xdg.configFile."gtk-4.0/assets" = {
+    source = "${pkgs.layan-gtk-theme}/share/themes/Layan-dark/gtk-4.0/assets";
+    recursive = true;
+  };
+
   gtk.font = {
     name = "Montserrat SemiBold";
     package = pkgs.montserrat;
