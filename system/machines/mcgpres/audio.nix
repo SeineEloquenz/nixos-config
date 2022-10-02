5d6638681f5315c47fb5c658559366341649672a
Alexander Linder <alexander.linder@mondcarion.group>
2022-10-02 09:47:02 +0200

    Fix mcgpres audio config

--- system/machines/mcgpres/audio.nix
+++ system/machines/mcgpres/audio.nix
@@ -1,30 +1,32 @@
 { config, pkgs, ... }:{
 
-  xdg.configFile."wireplumber/main.lua.d/51-alsa-disable.lua".text = ''
-    spdif = {
-      matches = {
-        {
-          { "node.nick", "equals", "ALC892 Digital" },
+  home-manager.users.alexa = {
+    xdg.configFile."wireplumber/main.lua.d/51-alsa-disable.lua".text = ''
+      spdif = {
+        matches = {
+          {
+            { "node.nick", "equals", "ALC892 Digital" },
+          },
         },
-      },
-      apply_properties = {
-        ["node.disabled"] = true,
-      },
-    }
-
-    camera = {
-      matches = {
-        {
-          { "node.nick", "equals", "HD Web Camera" },
-          { "media.class", "equals", "Audio/Source" },
+        apply_properties = {
+          ["node.disabled"] = true,
         },
-      },
-      apply_properties = {
-        ["node.disabled"] = true,
-      },
-    }
+      }
 
-    table.insert(alsa_monitor.rules,spdif)
-    table.insert(alsa_monitor.rules,camera)
-  '';
+      camera = {
+        matches = {
+          {
+            { "node.nick", "equals", "HD Web Camera" },
+            { "media.class", "equals", "Audio/Source" },
+          },
+        },
+        apply_properties = {
+          ["node.disabled"] = true,
+        },
+      }
+
+      table.insert(alsa_monitor.rules,spdif)
+      table.insert(alsa_monitor.rules,camera)
+    '';
+  };
 }
