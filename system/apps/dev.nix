74a482941f6386f4fe769456659c95ea6983b854
Alexander Linder <alexander.linder@mondcarion.group>
2022-10-04 20:17:36 +0200

    Migrate to non fhs codium

--- system/apps/dev.nix
+++ system/apps/dev.nix
@@ -2,9 +2,19 @@
   environment.systemPackages = with pkgs; [
     jetbrains.webstorm
     jetbrains.idea-ultimate
-    vscodium-fhs
     dbeaver
+    vscodium
     texlive.combined.scheme-full
     nix-prefetch-git
   ];
+
+  home-manager.users.alexa = {
+    programs.vscode = {
+      enable = true;
+      package = pkgs.vscodium;
+      extensions = with pkgs.vscode-extensions; [
+        redhat.vscode-yaml
+      ];
+    };
+  };
 }
