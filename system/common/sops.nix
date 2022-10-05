ec1b37811e5c7dcd6c794c5f7ff89bad3c987869
Alexander Linder <alexander.linder@mondcarion.group>
2022-10-05 21:21:21 +0200

    Fix sops

--- system/common/sops.nix
+++ system/common/sops.nix
@@ -4,7 +4,7 @@
   # This will add secrets.yml to the nix store
   # You can avoid this by adding a string to the full path instead, i.e.
   #sops.defaultSopsFile = "/root/.sops/secrets/example.yaml";
-  sops.defaultSopsFile = ../.secrets/secrets.yaml;
+  sops.defaultSopsFile = ../../.secrets/secrets.yaml;
   # This is using an age key that is expected to already be in the filesystem
   sops.age.keyFile = "/root/keys.txt";
 }
