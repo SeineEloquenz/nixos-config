57e122727f2e04b29437ea85830bd56ba242affd
Alexander Linder <alexander.linder@mondcarion.group>
2022-10-08 01:06:50 +0200

    Get zsh plugins from nixpkgs

--- system/common/zsh.nix
+++ system/common/zsh.nix
@@ -1,9 +1,14 @@
 { config, pkgs, lib, ... }:
 
 {
+
+  environment.pathsToLink = [ "/share/zsh" ];  
+
   home-manager.users.alexa = {
     programs.zsh = {
       enable = true;
+      enableSyntaxHighlighting = true;
+      enableAutosuggestions = true;
 
       shellAliases = {
         asystem = "sh ~/.dotfiles/scripts/apply-system.sh";
@@ -61,26 +66,6 @@
        '';
 
       plugins = [
-        {
-          name = "zsh-autosuggestions";
-          src = pkgs.fetchFromGitHub {
-            owner = "zsh-users";
-            repo = "zsh-autosuggestions";
-            rev = "v0.6.4";
-            sha256 = "0h52p2waggzfshvy1wvhj4hf06fmzd44bv6j18k3l9rcx6aixzn6";
-          };
-        }
-
-        {
-          name = "fast-syntax-highlighting";
-          src = pkgs.fetchFromGitHub {
-            owner = "zdharma";
-            repo = "fast-syntax-highlighting";
-            rev = "v1.55";
-            sha256 = "0h7f27gz586xxw7cc0wyiv3bx0x3qih2wwh05ad85bh2h834ar8d";
-          };
-        }
-
         {
           name = "powerlevel10k";
           src = pkgs.zsh-powerlevel10k;
