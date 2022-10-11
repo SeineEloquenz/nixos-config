1527e3a70e309ef27eb5eec8b1335fbb835d0d30
Alexander Linder <alexander.linder@mondcarion.group>
2022-10-11 17:35:16 +0200

    Make idrac run without fhs env

--- packages/idrac-6/idrac-6.nix
+++ packages/idrac-6/idrac-6.nix
@@ -5,7 +5,8 @@
 , buildFHSUserEnv
 , iDRAC
 , runCommand
-, libX11, libXext, libXi, libXt, libXtst, libXrender, freetype, fontconfig
+, autoPatchelfHook
+, alsa-lib, libX11, libXext, libXi, libXt, libXtst, libXrender, freetype, liberation_ttf, fontconfig
 }:
 
 let
@@ -15,8 +16,26 @@ let
       url = "https://cdn.azul.com/zulu/bin/zulu7.56.0.11-ca-jdk7.0.352-linux_x64.tar.gz";
       sha256 = "15xb1ahnqjmmi7688rspd8aqsqh9qcyfjqdj4yi2q2n9yk920rq3";
     };
+
+    buildInputs = [
+      autoPatchelfHook
+      alsa-lib
+      libX11
+      libXext
+      libXi
+      libXt
+      libXtst
+      libXrender
+      freetype
+      liberation_ttf
+      fontconfig
+    ];
+
     installPhase = ''
-      cp -r $src $out
+      mkdir -p $out/jre/lib/fonts
+      cp -r $src/* $out
+      # Link fallback font
+      ln -s ${liberation_ttf}/share/fonts/truetype $out/jre/lib/fonts/fallback
     '';
   };
  
@@ -29,22 +48,7 @@ let
     icon = ./src/share/icons/dell-logo.png;
   };
 
-  idrac-env = buildFHSUserEnv {
-    name = "idrac-6-env";
-    multiPkgs = pkgs: [
-      jre
-      libX11
-      libXext
-      libXi
-      libXt
-      libXtst
-      libXrender
-      freetype
-      fontconfig
-    ];
-  };
-
-  idrac = stdenv.mkDerivation rec {
+in stdenv.mkDerivation rec {
     name = "idrac-6";
     src = ./src;
 
@@ -53,30 +57,11 @@ let
       cp -r ${src}/lib $out
       cp -r ${src}/share $out
 
-      cat <<EOT >> $out/lib/iDRAC
-      java -cp $out/share/java/avctKVM.jar com.avocent.idrac.kvm.Main -Djava.security.debug=properties -Djava.library.path=$out/lib -Djava.security.properties==$out/share/java/java.security ip=${iDRAC.host} kmport=${iDRAC.port} vport=${iDRAC.port} user=${iDRAC.user} passwd="\$(< ~/.config/idrac-6/pw)" apcp=1 version=2 vmprivilege=true "helpurl=https://${iDRAC.host}:443/help/contents.html"
+      cat <<EOT >> $out/bin/iDRAC
+      ${jre}/bin/java -cp $out/share/java/avctKVM.jar com.avocent.idrac.kvm.Main -Djava.security.debug=properties -Djava.library.path=$out/lib -Djava.security.properties==$out/share/java/java.security ip=${iDRAC.host} kmport=${iDRAC.port} vport=${iDRAC.port} user=${iDRAC.user} passwd="\$(< ~/.config/idrac-6/pw)" apcp=1 version=2 vmprivilege=true "helpurl=https://${iDRAC.host}:443/help/contents.html"
       EOT
 
-      chmod +x $out/lib/iDRAC
-
-      cat <<EOT >> $out/bin/start.sh
-      #!${pkgs.bash}/bin/bash
-      $out/lib/iDRAC
-      EOT
-      chmod +x $out/bin/start.sh
-    '';
-  };
-
-in runCommand "iDRAC"
-  {
-    startScript = ''
-      #!${pkgs.bash}/bin/bash
-      ${idrac-env}/bin/idrac-6-env ${idrac}/bin/start.sh "$@"
+      chmod +x $out/bin/iDRAC
+      ln -s ${desktopItem}/share/applications $out/share/applications
     '';
   }
-  ''
-    mkdir -p $out/{bin,share}
-    echo -n "$startScript" > $out/bin/iDRAC
-    chmod +x $out/bin/iDRAC
-    ln -s ${desktopItem}/share/applications $out/share/applications
-  ''
