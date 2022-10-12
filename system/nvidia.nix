fb3b0ec13be191f5b07982db5386b49ac8f4f024
Alexander Linder <alexander.linder@mondcarion.group>
2022-10-12 16:11:40 +0200

    Reenable persistenced

--- system/nvidia.nix
+++ system/nvidia.nix
@@ -1,7 +1,10 @@
 { config, pkgs, ... }:{
 
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia = {
+    modesetting.enable = true;
+    nvidiaPersistenced = true;
+  };
   hardware.opengl.enable = true;
   hardware.opengl.driSupport32Bit = true;
 
