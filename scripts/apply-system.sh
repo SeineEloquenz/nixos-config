c5a259cadb35dda19e3518142a5445fbe7b8c615
Alexander Linder <alexander.linder@mondcarion.group>
2022-10-12 02:23:35 +0200

    Reorder commands in apply script

--- scripts/apply-system.sh
+++ scripts/apply-system.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 pushd ~/.dotfiles
-sudo nixos-rebuild build --flake . && cachix push mcg-cache result && sudo nixos-rebuild switch --flake . $@
+sudo nixos-rebuild build --flake . && sudo nixos-rebuild switch --flake . $@ && cachix push mcg-cache result
 popd
