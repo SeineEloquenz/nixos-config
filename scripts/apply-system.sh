98df15abac3d11d7be781b211b1ad3b580300628
Alexander Linder <alexander.linder@mondcarion.group>
2022-10-17 22:14:33 +0200

    Restructure apply script

--- scripts/apply-system.sh
+++ scripts/apply-system.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 if [ "$1" == "-c" ]; then
-arg="--option extra-substituters http://nix-cache.internal.mondcarion.group:5000"
-nix copy -s --from https://cache.nixos.org --to ssh://nix-cache $(nixos-rebuild dry-build --flake . |& sed '0,/^these .* paths will be fetched .*$/d')
+  arg="--option extra-substituters http://nix-cache.internal.mondcarion.group:5000"
+  pkgs=nixos-rebuild dry-build --flake . |& sed '0,/^these .* paths will be fetched .*$/d'
+  if [ "$pkgs" != "" ]; then
+    nix copy -s --from https://cache.nixos.org --to ssh://nix-cache $pkgs
+  fi
 else
-arg=""
+  arg=""
 fi
 pushd ~/.dotfiles
-nixos-rebuild build $arg --flake . && sudo nixos-rebuild switch --flake .
-if [ "$1" == "-c" ]; then
-sh ./scripts/cache-system.sh
-fi
+  nixos-rebuild build $arg --flake . && sudo nixos-rebuild switch --flake .
+  if [ "$1" == "-c" ]; then
+    sh ./scripts/cache-system.sh
+  fi
 popd
