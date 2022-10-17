e879260651d8524916baf960d01a258d6868f833
Alexander Linder <alexander.linder@mondcarion.group>
2022-10-17 22:03:28 +0200

    Update asystem script

--- scripts/apply-system.sh
+++ scripts/apply-system.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 if [ "$1" == "-c" ]; then
 arg="--option extra-substituters http://nix-cache.internal.mondcarion.group:5000"
+nix copy -s --from https://cache.nixos.org --to ssh://nix-cache $(nixos-rebuild dry-build --flake . |& sed '0,/^these .* paths will be fetched .*$/d')
 else
 arg=""
 fi
