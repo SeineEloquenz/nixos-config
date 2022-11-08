6561d0e1ea0ed71bbaed112a593b5987859da27e
Alexander Linder <alexander.linder@mondcarion.group>
2022-11-08 15:34:11 +0100

    Refactor bootstrap script

--- bootstrap.nix
+++ bootstrap.nix
@@ -1,11 +1,16 @@
 with (import <nixpkgs> {});
-mkShell {
+
+let
+  email = "alexander.linder@mondcarion.group";
+  base_url = "https://pass.mondcarion.group";
+  lock_timeout = "30";
+in mkShell {
   buildInputs = [
     rbw
   ];
   shellHook = ''
-    rbw config set "email" "alexander.linder@mondcarion.group"
-    rbw config set "base_url" "https://pass.mondcarion.group"
-    rbw config set "lock_timeout": 30
+    rbw config set "email" "${email}"
+    rbw config set "base_url" "${base_url}"
+    rbw config set "lock_timeout" ${lock_timeout}
   '';
 }
