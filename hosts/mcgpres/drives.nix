9b2acb9bd799a9c8db752cfc239a47a703b7e1e2
Alexander Linder <alexander.linder@mondcarion.group>
2022-11-09 22:33:55 +0100

    Update mcgpres for amd system

--- hosts/mcgpres/drives.nix
+++ hosts/mcgpres/drives.nix
@@ -8,14 +8,6 @@
 
   boot.initrd.luks.devices."luks-cb69389d-8118-4c2a-af8a-6d92848d6bad".device = "/dev/disk/by-uuid/cb69389d-8118-4c2a-af8a-6d92848d6bad";
 
-  # IData
-  fileSystems."/run/media/alexa/IData" = {
-      device = "/dev/disk/by-uuid/bc36ed71-c04b-4611-8436-490fdae64af6";
-      fsType = "ext4";
-    };
-
-  boot.initrd.luks.devices."luks-08a66126-62ed-4ffe-a8dd-ad7a9c61f29b".device = "/dev/disk/by-uuid/08a66126-62ed-4ffe-a8dd-ad7a9c61f29b";
-
   # Data
   fileSystems."/run/media/alexa/Data" = {
       device = "/dev/disk/by-uuid/29aea319-492f-4a78-99e0-85d2147cb727";
