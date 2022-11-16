418f457277d209db7cb17a3d0115ddbfee464200
Alexander Linder <alexander.linder@mondcarion.group>
2022-11-16 23:46:32 +0100

    Add iommu setting

--- hosts/mcgpres/configuration.nix
+++ hosts/mcgpres/configuration.nix
@@ -26,6 +26,11 @@
     ];
   };
 
+  boot.kernelParams = [
+    "iommu=pt"
+    "amd_iommu=on"
+  ];
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
