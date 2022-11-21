a00ddb1e5db794f3c3a4c83b16ca4fa40af421aa
Alexander Linder <alexander.linder@mondcarion.group>
2022-11-21 13:05:32 +0100

    Make help flag an operation in config

--- pkgs/config/completion.zsh
+++ pkgs/config/completion.zsh
@@ -1,6 +1,5 @@
 #compdef config
 
 _arguments \
-    "--help" \
     "--cache" \
-    "1: :(apply cache build build-all pull push status update)"
+    "1: :(apply cache build build-all pull push status update help)"
