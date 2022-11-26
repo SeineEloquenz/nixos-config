7c677bf8e6f674a228d6c5c9506a7f7416d32f46
Alexander Linder <alexander.linder@mondcarion.group>
2022-11-26 17:20:21 +0100

    Fix mimetype for vscode

--- system/apps/dev.nix
+++ system/apps/dev.nix
@@ -10,22 +10,16 @@ let
   });
 
 in {
+
+  imports = [
+    ./vscode.nix
+  ];
+
   environment.systemPackages = with pkgs; [
     jetbrains.webstorm
     jetbrains.idea-ultimate
     dbeaver
-    vscodium
     mytexlive
     nix-prefetch-git
   ];
-
-  home-manager.users.alexa = {
-    programs.vscode = {
-      enable = true;
-      package = pkgs.vscodium;
-      extensions = with pkgs.vscode-extensions; [
-        redhat.vscode-yaml
-      ];
-    };
-  };
 }
---
+++ system/apps/vscode.nix
@@ -0,0 +1,39 @@
+{ pkgs, config, ... }:
+
+{
+  environment.systemPackages = [
+    pkgs.vscodium
+  ];
+
+  home-manager.users.alexa = {
+    programs.vscode = {
+      enable = true;
+      package = pkgs.vscodium;
+      extensions = with pkgs.vscode-extensions; [
+        redhat.vscode-yaml
+      ];
+    };
+
+    home.file.".local/share/applications/codium.desktop".text = ''
+      [Desktop Entry]
+      Actions=new-empty-window
+      Categories=Utility;TextEditor;Development;IDE
+      Comment=Code Editing. Redefined.
+      Exec=codium %F
+      GenericName=Text Editor
+      Icon=code
+      Keywords=vscode
+      MimeType=text/plain
+      Name=VSCodium
+      StartupNotify=true
+      StartupWMClass=vscodium
+      Type=Application
+      Version=1.4
+
+      [Desktop Action new-empty-window]
+      Exec=codium --new-window %F
+      Icon=code
+      Name=New Empty Window
+    '';
+  };
+}
