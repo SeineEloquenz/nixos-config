495baf374c77a5dd8ea7b0d7470d1a14f96ddf25
Alexander Linder <alexander.linder@mondcarion.group>
2022-12-21 00:34:00 +0100

    Refactor indentation

--- flake.nix
+++ flake.nix
@@ -23,56 +23,56 @@
   };
 
   outputs = inputs@{ self, nixpkgs, sops-nix, home-manager, juso-motion-template, idrac-6 }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config = {
-          allowUnfree = true;
-        };  
-        overlays = [
-          (import ./overlays/layan.nix)
-          (import ./overlays/nextcloud-client.nix)
-          (import ./pkgs)
-          ( self: super:  {
-            juso-motion-template = inputs.juso-motion-template.packages.${system}.juso-motion-template;
-          })
-        ];
-      };
-      lib = nixpkgs.lib;
-      defaultModules = [
-        sops-nix.nixosModules.sops
-        idrac-6.nixosModules.idrac-6
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-        }
-        # Our modules
-        ./modules/kubernetes.nix
-        ./modules/ssh-keys.nix
-        ./modules/networks.nix
-        ./modules/prismlauncher.nix
-        # Our packages
-        ./pkgs/steam-adwaita
-        # Common Components
-        ./system/common
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
+      };  
+      overlays = [
+        (import ./overlays/layan.nix)
+        (import ./overlays/nextcloud-client.nix)
+        (import ./pkgs)
+        ( self: super:  {
+          juso-motion-template = inputs.juso-motion-template.packages.${system}.juso-motion-template;
+        })
       ];
-      mkSystem = name: nixpkgs.lib.nixosSystem {
-        inherit system pkgs;
-        modules = defaultModules ++ [
-          ./hosts/${name}/configuration.nix
-          ./hosts/${name}/hardware-configuration.nix
-          {
-            networking.hostName = name;
-          }
-        ];
-      };
-    in {
-      nixosConfigurations = {
-        mcggenesis = mkSystem "mcggenesis";
-        mcgpres = mkSystem "mcgpres";
-        mcgzen = mkSystem "mcgzen";
-        surface = mkSystem "surface";
-      };
     };
+    lib = nixpkgs.lib;
+    defaultModules = [
+      sops-nix.nixosModules.sops
+      idrac-6.nixosModules.idrac-6
+      home-manager.nixosModules.home-manager {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+      }
+      # Our modules
+      ./modules/kubernetes.nix
+      ./modules/ssh-keys.nix
+      ./modules/networks.nix
+      ./modules/prismlauncher.nix
+      # Our packages
+      ./pkgs/steam-adwaita
+      # Common Components
+      ./system/common
+    ];
+    mkSystem = name: nixpkgs.lib.nixosSystem {
+      inherit system pkgs;
+      modules = defaultModules ++ [
+        ./hosts/${name}/configuration.nix
+        ./hosts/${name}/hardware-configuration.nix
+        {
+          networking.hostName = name;
+        }
+      ];
+    };
+  in {
+    nixosConfigurations = {
+      mcggenesis = mkSystem "mcggenesis";
+      mcgpres = mkSystem "mcgpres";
+      mcgzen = mkSystem "mcgzen";
+      surface = mkSystem "surface";
+    };
+  };
 }
